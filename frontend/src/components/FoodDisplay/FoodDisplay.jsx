05a135f753fdaa5614940933405a3e365648f980
Jeewapadma <udithjeewapadma117@gmail.com>
2024-08-28 20:17:06 +0530

    filtered and display food items from the category

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -10,7 +10,10 @@ const FoodDisplay = ({category}) => {
         <h2>Top Dishes For You</h2>
         <div className="food-display-list">
             {food_list.map((item, index) =>{
+              if(category === 'All' || category===item.category){
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+
+              }
             })}
         </div>
         
