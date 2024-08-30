0a4ffdeab1435e5d0b68eefbcacffdda1aab4b7b
Jeewapadma <udithjeewapadma117@gmail.com>
2024-08-30 15:53:17 +0530

    create and style footer component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='app'>
+    <>
+      <div className='app'>
       <Navbar />
       <Routes>
         <Route path='/' element= {<Home />} />
@@ -16,6 +18,10 @@ const App = () => {
 
       </Routes>
     </div>
+    <Footer />
+
+    </>
+
   )
 }
 
