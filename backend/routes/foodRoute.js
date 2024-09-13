c6ea91e08e8a0c79e6617ad05102fe39196a48b4
Jeewapadma <udithjeewapadma117@gmail.com>
2024-09-13 14:36:20 +0530

    create listFood functionality

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addFood } from '../controllers/foodController.js'
+import { addFood, listFood } from '../controllers/foodController.js'
 import multer from 'multer'
 
 
@@ -16,7 +16,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
-foodRouter.post("/add", upload.single("image"),addFood )
+foodRouter.post("/add", upload.single("image"),addFood );
+foodRouter.get("/list", listFood)
 
 
 export default foodRouter;
